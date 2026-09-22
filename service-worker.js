066fd8a4e7f2cbfe26e9659e69e34521f80689c8
almansor97 <gerosmails@gmail.com>
2026-09-22 04:03:21 +0200

    Aktualisiere PWA-Cache

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="morgenbrief-v1";
-const SHELL=["./","./index.html","./manifest.webmanifest","./icons/icon.svg"];
+const CACHE="morgenbrief-v2";
+const SHELL=["./","./index.html","./manifest.webmanifest","./icons/icon.svg","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
 self.addEventListener("fetch",e=>{const u=new URL(e.request.url);if(u.pathname.includes("/data/")){e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));return}e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)))});
